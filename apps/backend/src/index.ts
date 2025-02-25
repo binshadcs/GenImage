32dd80d0e5f8dc0ac52fd6e785646a6bf4495b21
Binshad K <binshadcs@gmail.com>
2025-02-25 20:21:43 +0530

    feat: backend end point addded

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { GenerateImage, GenerateImageFromPack, TrainModel } from "common/types";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=> {
-    console.log(`Server is running ${PORT}`);
+app.post("/ai/training", (req: Request, res: Response) => {
+  res.send("Training end point");
+});
+
+app.post("/ai/generate", (req: Request, res: Response) => {
+  res.send("Generate end point");
+});
+
+app.post("/pack/generate", (req: Request, res: Response) => {
+  res.send("Pack generate end point");
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is up!");
+});
+
+app.get("/pack/bulk", (req: Request, res: Response) => {
+  res.send("Bulk pack end point");
+});
+
+app.get("/image", (req: Request, res: Response) => {
+  res.send("Get image");
+});
+
+app.listen(3001, () => {
+  console.log(`Server is running ${PORT}`);
 });
